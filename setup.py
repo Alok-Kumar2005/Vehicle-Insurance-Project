a757874fcb9a1f9ca6875b9d0020323365ac4656
Alok-Kumar2005 <ay747283@gmail.com>
2024-12-15 00:15:32 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Alok Kumar",
+    author_email="ay747283@gmail.com",
+    packages=find_packages()
+)
